36ad61e51616858390172d586e7e0f1e67a288b8
Papanna <apapanna@in.imshealth.com>
2020-07-04 17:11:01 +0530

    removed dist

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/CI-P0'));
 
-app.get('/', function(req,res) {
+app.get('/*', function(req,res) {
     
     
 res.sendFile(path.join(__dirname,'/dist/CI-P0/index.html'));
